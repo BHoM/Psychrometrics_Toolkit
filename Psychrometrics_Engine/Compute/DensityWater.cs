248671afec5cc5ffd00c1c50a29e6a4062c17d88
Ellie Adebowale <ellie.adebowale@burohappold.com>
2023-03-16 17:32:26 +0000

    maybe what was needed? Moving methods

---
+++ Psychrometrics_Engine/Compute/DensityWater.cs
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using BH.oM.Base.Attributes;
+
+namespace BH.Engine.Psychrometrics
+{
+    public static partial class Compute
+    {
+        [Description("Calculates water density from temperature")]
+        [Input("temperature", "temperature [degC]")]
+        [Output("density", "Density [kg/m3]")]
+        public static double DensityWater(double temperature)
+        {
+            double t = temperature;
+            if (t < 0 || t > 150)
+            {
+                //BH.Engine.Base.Compute.RecordError("Temperature must be greater than 0 and less than 150 degC");
+                return -1;
+            }
+            else
+            {
+                return 999.792764532729 + 0.07544354069978 * t - 8.88812233461067e-03 * Math.Pow(t, 2) + 7.43496157156187e-05 * Math.Pow(t, 3) - 5.05819372165206e-07 * Math.Pow(t, 4) + 1.71286251848812e-09 * Math.Pow(t, 5) - 3.41712769191815e-13 * Math.Pow(t, 6) - 8.9940922954777e-15 * Math.Pow(t, 7);
+            }
+        }
+    }
+}
---
+++ Psychrometrics_Engine/Compute/SaturatedVapourPressureWater.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+using BH.oM.Base.Attributes;
+
+namespace BH.Engine.Psychrometrics
+{
+    public static partial class Compute
+    {
+        [Description("Calculates water SaturatedVapourPressure from temperature")]
+        [Input("temperature", "temperature [degC]")]
+        [Output("saturatedVapourPressure", "Saturated Vapour Pressure [Pa]")]
+        public static double SaturatedVapourPressureWater(double temperature)
+        {
+            //BH.Engine.Base.Compute.RecordWarning("This method has not been thoroughly tested. The output may be incorrect. Use at own risk.");
+
+            double t = temperature;
+            if (t < 0 || t > 150)
+            {
+                //BH.Engine.Base.Compute.RecordError("Temperature must be greater than 0 and less than 150 degC");
+                return -1;
+            }
+            else if (temperature < 21)
+            {
+                return 6.10830198582769e-03 + 3.69554702125838e-04 * t + 2.4671509929139e-05 * Math.Pow(t, 2);
+            }
+            else
+            {
+                return 2.76521518826485e-02 - 1.9984832033515e-03 * t + 1.26386221381836e-04 * Math.Pow(t, 2) - 2.43248314291122E-06 * Math.Pow(t, 3) + 3.97667179186101E-08 * Math.Pow(t, 4) - 2.63438493242063e-10 * Math.Pow(t, 5) + 1.23191485224688e-12 * Math.Pow(t, 6) - 2.20158156672378E-15 * Math.Pow(t, 7);
+            }
+        }
+    }
+}
