cb2a8a21fdf96aad973d9bfbc4ab33ab7c5b825d
Albin Bergh <albin.bergh@burohappold.com>
2023-09-05 12:13:20 +0100

    Update Psychrometrics_Engine/Compute/FromHumidityRatio.cs
    Co-authored-by: Fraser Greenroyd <fraser.greenroyd@burohappold.com>

--- Psychrometrics_Engine/Compute/FromHumidityRatio.cs
+++ Psychrometrics_Engine/Compute/FromHumidityRatio.cs
@@ -39,7 +39,7 @@ namespace BH.Engine.Psychrometrics
         [Description("Calculates density, enthalpy, dew-point temperature, relative humidity, specific volume and wet-bulb temperature from dry-bulb temperature, pressure and humidity ratio.")]
         [Input("dryBulbTemperature", "Dry-bulb temperature.", typeof(Temperature))]
         [Input("humidityRatio", "Humidity ratio (kg_water/kg_dryair).", typeof(Ratio))]
-        [Input("pressure", "Air pressure, defaults to sea level air pressure (101,325).",typeof(Pressure))]
+        [Input("pressure", "Air pressure, defaults to sea level air pressure (101,325).", typeof(Pressure))]
         [MultiOutput(0, "density", "Density.", typeof(Density))]
         [MultiOutput(1, "enthalpy", "Enthalpy.",typeof(SpecificEnergy))]
         [MultiOutput(2, "dewPoint", "Dew-point temperature.", typeof(Temperature))]
